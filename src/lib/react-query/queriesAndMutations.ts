9775b943cdf7b5bda36d1280253f121af1f16ef0
Abhijit Pati <95014101+abhijitpati11@users.noreply.github.com>
2024-02-04 19:50:39 +0530

    Users can now view their and other profiles, the profile page has separate categories to see own posts and all liked post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -8,7 +8,7 @@ import {
 } from '@tanstack/react-query';
 
 
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePost, getPostById, getRecentPosts, getUsers, likePost, savePost, searchPost, signInAccount, signOutAccount, updatePost } from '../appwrite/api'
+import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePost, getPostById, getRecentPosts, getUserById, getUsers, likePost, savePost, searchPost, signInAccount, signOutAccount, updatePost } from '../appwrite/api'
 import { INewPost, INewUser, IUpdatePost } from '@/types';
 import { CreatePost } from '@/_root/pages';
 import { QUERY_KEYS } from './queryKeys';
@@ -206,4 +206,12 @@ export const useGetUsers = (limit?: number) => {
    queryKey: [QUERY_KEYS.GET_USERS],
    queryFn: () => getUsers(limit),
  });
+};
+
+export const useGetUserById = (userId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_USER_BY_ID, userId],
+    queryFn: () => getUserById(userId),
+    enabled: !!userId,
+  });
 };
